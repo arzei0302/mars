40c69073fc43bafb4c9514125b2e8024d094d1d6
arzei0302 <kabulov.arz@gmail.com>
2024-05-07 19:24:43 +0600

    добавлено с гитхаба

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # MARS project
+
+python3 manage.py startapp mars_api
+
+# миграции
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+чтобы создать новую БД в консоле пишем:
+
+**createdb mara_db**
+
+удалить БД в консоле пишем:
+**dropdb mars_db**
+
+# админка
+
+создание суперпользователя:
+
+**python3 manage.py createsuperuser**
--- mars_servise/mars_servise/settings.py
+++ mars_servise/mars_servise/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'orders_app'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +74,20 @@ WSGI_APPLICATION = 'mars_servise.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mars_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -103,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
